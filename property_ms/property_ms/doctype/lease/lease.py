c448a75a67a974e99e816f2920d38d642aa7fe3d
umair2004 <mumairfqabbasi812@gmail.com>
2025-06-27 18:14:08 +0500

    update lease

--- property_ms/property_ms/doctype/lease/lease.py
+++ property_ms/property_ms/doctype/lease/lease.py
@@ -5,7 +5,8 @@ import frappe
 from frappe.utils import flt, nowdate
 from frappe.model.document import Document
 import json
-
+from erpnext.stock.get_item_details import _get_item_tax_template
+from erpnext.controllers.taxes_and_totals import get_itemised_tax
 
 
 class Lease(Document):
@@ -49,102 +50,255 @@ def terminate_lease(doc, terminated_date):
 	frappe.msgprint("Lease has been terminated successfully")
 
 
+# @frappe.whitelist()
+# def make_sales_invoice_scheduler():
+# 	try:
+# 		lease_list = frappe.get_all("Lease", filters=[['enabled', '=', 1], ['docstatus', '=', 1]])
+# 		for lease in lease_list:
+# 			try:
+# 				total_unit_amount = 0
+# 				doc = frappe.get_doc('Lease', lease.name)
+# 				total_unit_amount += sum(unit.yearly for unit in doc.choose_units)
+# 				for payment in doc.payments_scheduling:
+# 					try:
+# 						additional_charges = payment.additional_charges / len(doc.choose_units)
+# 						if str(payment.issued_date) <= nowdate() and payment.invoiced == 0:
+# 							frappe.db.set_value("Payments Scheduling", payment.name, 'invoiced', 1)
+# 							# rate_wo_tax = flt(payment.total_amount) - flt(payment.total_tax)
+							
+# 							for tenant in doc.property_ownership:
+# 								try:
+# 									invoice = frappe.get_doc({
+# 										"doctype": "Sales Invoice",
+# 										"customer": doc.renter,
+# 										"set_posting_time": 1,
+# 										"posting_date": payment.issued_date,
+# 										"due_date": payment.due_date,
+# 										"company": doc.company,
+# 										"custom_lease_reference": doc.name
+# 									})
+
+# 									for unit in doc.choose_units:
+# 										itemRate = 0
+# 										if not doc.ex_tax_on_add_char:
+# 											itemRate = ((unit.yearly / total_unit_amount) * (payment.rent_amount + payment.additional_charges)) # total_amount <-- rent_amount + additional_charges 
+# 										else:
+# 											itemRate = ((unit.yearly / total_unit_amount) * payment.rent_amount)
+
+# 										invoice.append("items", {
+# 											"item_code": doc.item,
+# 											"qty": 1,
+# 											"rate": itemRate * (tenant.ownership / 100),
+# 											"cost_center": tenant.cost_center,	
+# 											"properties": doc.property_name,
+# 											"unit_center": unit.unit_name,
+# 											# "income_account": income_account.name	
+# 										})
+
+
+# 										if doc.ex_tax_on_add_char:
+# 											if additional_charges > 0:
+# 												additional_charges_item = get_additional_item()
+# 												invoice.append("items", {
+# 													"item_code": additional_charges_item.item_code,
+# 													"qty": 1,
+# 													"rate": additional_charges * (tenant.ownership / 100),
+# 													"cost_center": tenant.cost_center,
+# 													"properties": doc.property_name,
+# 													"unit_center": unit.unit_name,
+# 												})
+# 											for lease_tax in doc.taxes:
+# 												itemRate = 0
+# 												for item in invoice.items:
+# 													if item.item_code == doc.item:
+# 														itemRate = item.rate
+
+# 												tax_amount = (itemRate / 100) * lease_tax.rate
+# 												if tax_amount > 0:
+# 													invoice.append("taxes", {
+# 														"charge_type": "Actual",
+# 														"account_head": lease_tax.account_head,
+# 														"rate": lease_tax.rate,
+# 														"description": lease_tax.description,
+# 														"cost_center": lease_tax.cost_center,
+# 														"tax_amount": tax_amount
+# 													})
+# 										else:
+# 											invoice.taxes_and_charges = doc.taxes_and_charges
+# 											for lease_tax in doc.taxes:
+# 												invoice.append("taxes", {
+# 													"charge_type": lease_tax.charge_type,
+# 													"account_head": lease_tax.account_head,
+# 													"rate": lease_tax.rate,
+# 													"description": lease_tax.description,
+# 													"cost_center": lease_tax.cost_center,
+# 													# "tax_amount": (itemRate / 100) * lease_tax.rate,
+# 												})
+# 									invoice.set_missing_values()
+# 									invoice.calculate_taxes_and_totals()
+# 									invoice.insert()
+# 								except Exception as tenant_err:
+# 									frappe.log_error(f"Error creating invoice for tenant in lease {lease.name}: {str(tenant_err)}")
+# 					except Exception as payment_err:
+# 						frappe.log_error(f"Error processing payment schedule for lease {lease.name}: {str(payment_err)}")
+# 			except Exception as lease_err:
+# 				frappe.log_error(f"Error processing lease {lease.name}: {str(lease_err)}")
+# 	except Exception as e:
+# 		frappe.log_error(f"Unexpected error in make_sales_invoice_scheduler: {str(e)}")
+
+
 @frappe.whitelist()
 def make_sales_invoice_scheduler():
 	try:
 		lease_list = frappe.get_all("Lease", filters=[['enabled', '=', 1], ['docstatus', '=', 1]])
 		for lease in lease_list:
 			try:
+				check = "start"
 				total_unit_amount = 0
 				doc = frappe.get_doc('Lease', lease.name)
 				total_unit_amount += sum(unit.yearly for unit in doc.choose_units)
+
+				additional_item = get_additional_item()
+				additional_item_code = additional_item.item_code
+				zero_tax_acc = None
+				zero_tax_acc_si_tax = None
+				# rent_vat_acc = doc.taxes[0].account_head if doc.taxes else None
+				tax_rate = doc.taxes[0].rate if doc.taxes else 0
+				rent_result = frappe.db.sql("""
+										SELECT ittd.parent
+										FROM `tabItem Tax Template Detail` ittd
+										JOIN `tabItem Tax Template` itt ON itt.name = ittd.parent
+										WHERE ittd.parenttype = 'Item Tax Template'
+											AND ittd.tax_rate = %s
+											AND itt.company = %s
+										LIMIT 1
+									""", (tax_rate, doc.company), as_dict=True)
+				rent_vat_acc = rent_result[0]["parent"] if rent_result else None
+				currency = frappe.db.get_value('Company', doc.company, 'default_currency')
+				charge_type = doc.taxes[0].charge_type if doc.taxes else "On Net Total"
 				for payment in doc.payments_scheduling:
 					try:
 						additional_charges = payment.additional_charges / len(doc.choose_units)
 						if str(payment.issued_date) <= nowdate() and payment.invoiced == 0:
 							frappe.db.set_value("Payments Scheduling", payment.name, 'invoiced', 1)
-							rate_wo_tax = flt(payment.total_amount) - flt(payment.total_tax)
-							
+
 							for tenant in doc.property_ownership:
 								try:
-									invoice = frappe.get_doc({
-										"doctype": "Sales Invoice",
-										"customer": doc.renter,
-										"set_posting_time": 1,
-										"posting_date": payment.issued_date,
-										"due_date": payment.due_date,
-										"company": doc.company,
-										"custom_lease_reference": doc.name,
-									})
+									invoice = frappe.new_doc("Sales Invoice")
+									invoice.customer = doc.renter
+									invoice.set_posting_time = 1
+									invoice.posting_date = payment.issued_date
+									invoice.due_date = payment.due_date
+									invoice.company = doc.company
+									invoice.custom_lease_reference = doc.name
+									invoice.disable_rounded_total = 0
 
 									for unit in doc.choose_units:
 										itemRate = 0
-										if doc.ex_tax_on_add_char == 0:
-											itemRate = ((unit.yearly / total_unit_amount) * payment.total_amount) # total_amount <-- rent_amount + additional_charges + tax_amount
+										if not doc.ex_tax_on_add_char:
+											itemRate = ((unit.yearly / total_unit_amount) * (payment.rent_amount + payment.additional_charges))
 										else:
 											itemRate = ((unit.yearly / total_unit_amount) * payment.rent_amount)
+										result = frappe.db.sql("""
+											SELECT
+								 				ittd.parent
+								 								
+											FROM `tabItem Tax Template Detail` ittd
+											JOIN `tabItem Tax Template` itt ON itt.name = ittd.parent
+											WHERE ittd.parenttype = 'Item Tax Template'
+												AND ittd.tax_rate = %s
+												AND itt.company = %s
+											LIMIT 1
+										""", (0, doc.company), as_dict=True)
+										if doc.expenses and doc.expenses[0].item_tax_template:
+											zero_tax_acc = doc.expenses[0].item_tax_template
+										elif result:
+											zero_tax_acc = result[0]["parent"]
+										if zero_tax_acc:
+											acc_doc = frappe.get_doc("Item Tax Template", zero_tax_acc)
+											if acc_doc and acc_doc.taxes:
+												zero_tax_acc_si_tax = acc_doc.taxes[0].tax_type if acc_doc.taxes else None
+										
+											
 
-										invoice.append("items", {
+										rent_item = {
 											"item_code": doc.item,
 											"qty": 1,
 											"rate": itemRate * (tenant.ownership / 100),
-											"cost_center": tenant.cost_center,	
+											"cost_center": tenant.cost_center,
 											"properties": doc.property_name,
-											"unit_center": unit.unit_name,
-											# "income_account": income_account.name	
-										})
+											"unit_center": unit.unit_name
+										}
+
+										if rent_vat_acc:
+											rent_item["item_tax_template"] = rent_vat_acc
+
+										invoice.append("items", rent_item)
 
 
-										if doc.ex_tax_on_add_char == 1:
+										if doc.ex_tax_on_add_char:
 											if additional_charges > 0:
-												additional_charges_item = get_additional_item()
-												invoice.append("items", {
-													"item_code": additional_charges_item.item_code,
+												add_item = {
+													"item_code": additional_item_code,
 													"qty": 1,
 													"rate": additional_charges * (tenant.ownership / 100),
 													"cost_center": tenant.cost_center,
 													"properties": doc.property_name,
-													"unit_center": unit.unit_name,
-												})
-											for lease_tax in doc.taxes:
-												itemRate = 0
-												for item in invoice.items:
-													if item.item_code == doc.item:
-														itemRate = item.rate
+													"unit_center": unit.unit_name
+												}
 
-												tax_amount = (itemRate / 100) * lease_tax.rate
-												if tax_amount > 0:
-													invoice.append("taxes", {
-														"charge_type": "Actual",
-														"account_head": lease_tax.account_head,
-														"rate": lease_tax.rate,
-														"description": lease_tax.description,
-														"cost_center": lease_tax.cost_center,
-														"tax_amount": tax_amount
-													})
-										else:
-											invoice.taxes_and_charges = doc.taxes_and_charges
-											for lease_tax in doc.taxes:
+												if zero_tax_acc:
+													add_item["item_tax_template"] = zero_tax_acc
+												invoice.append("items", add_item)
+									try:
+										invoice.set_missing_values()
+										invoice.flags.ignore_permissions = True
+										invoice.set("taxes", [])
+
+										exis_temp = []
+
+										for item in invoice.items:
+											if not item.item_tax_template or item.item_tax_template in exis_temp:
+												continue
+											template = frappe.get_doc("Item Tax Template", item.item_tax_template)
+											exis_temp.append(item.item_tax_template)
+
+											for tax_row in template.taxes:
 												invoice.append("taxes", {
-													"charge_type": lease_tax.charge_type,
-													"account_head": lease_tax.account_head,
-													"rate": lease_tax.rate,
-													"description": lease_tax.description,
-													"cost_center": lease_tax.cost_center,
-													# "tax_amount": (itemRate / 100) * lease_tax.rate,
+													"charge_type": charge_type,
+													"account_head": tax_row.tax_type,
+													# "rate": tax_row.tax_rate,
+													"rate": 0,
+													"description": tax_row.tax_type,
+													"cost_center": doc.taxes[0].cost_center or item.cost_center or invoice.company_default_cost_center,
+													"account_currency": currency,
+													"tax_amount": payment.total_tax if tax_row.tax_rate and tax_row.tax_rate != 0 else 0
 												})
+
 										
-									invoice.set_missing_values()
-									invoice.calculate_taxes_and_totals()
-									invoice.insert()
+										invoice.calculate_taxes_and_totals()
+										invoice.insert()
+										# invoice.save()
+									except Exception as e:
+										frappe.log_error(f"Validation error while creating invoice for tenant in lease {lease.name}: {str(e)}")
+										# print(f"[ERROR] Validation error while creating invoice for tenant in lease {lease.name}: {str(ve)}")
+									
 								except Exception as tenant_err:
 									frappe.log_error(f"Error creating invoice for tenant in lease {lease.name}: {str(tenant_err)}")
+									# print(f"[ERROR] Creating invoice for tenant in lease {lease.name}: {str(tenant_err)}")
+
 					except Exception as payment_err:
 						frappe.log_error(f"Error processing payment schedule for lease {lease.name}: {str(payment_err)}")
+						# print(f"[ERROR] Processing payment schedule for lease {lease.name}: {str(payment_err)}")
 			except Exception as lease_err:
 				frappe.log_error(f"Error processing lease {lease.name}: {str(lease_err)}")
+				# print(f"[ERROR] Processing lease {lease.name}: {str(lease_err)}")
+
 	except Exception as e:
 		frappe.log_error(f"Unexpected error in make_sales_invoice_scheduler: {str(e)}")
+		# print(f"[ERROR] Unexpected error in make_sales_invoice_scheduler: {str(e)}")
+
+
 
 @frappe.whitelist()
 def get_additional_item():
